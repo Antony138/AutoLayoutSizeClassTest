30445f7f78549d8b4d649331d969b4786b7ed6a8
Antony.Wong <aeq2005@163.com>
2015-10-19 09:42:17 +0800

    Init

---
+++ AutoLayoutSizeClassTest/AutoLayoutSizeClassTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutSizeClassTest
+//
+//  Created by YuHeng_Antony on 10/19/15.
+//  Copyright © 2015 Homni Electron Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutSizeClassTest/AutoLayoutSizeClassTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutSizeClassTest
+//
+//  Created by YuHeng_Antony on 10/19/15.
+//  Copyright © 2015 Homni Electron Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
